c2616966e7164f10a82ab62fdcba9e7030044ea1
Vab Gupta <vabgupta89@gmail.com>
2024-11-28 16:01:59 -0500

    feat: Added a unittest for weather api response

---
+++ src/test/java/ApiTests/WeatherApiTest.java
@@ -0,0 +1,26 @@
+
+package ApiTests;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import frontend.model.Weather;
+import frontend.utils.ApiUtils;
+
+public class WeatherApiTest {
+
+    @Test
+    void testFetchCurrentWeatherRealApi() {
+        final Weather weather = ApiUtils.fetchCurrentWeather();
+
+        // Ensure the response is not null
+        assertNotNull(weather);
+
+        // Ensure the values are within a valid range
+        assertTrue(weather.getTemperature() >= -50 && weather.getTemperature() <= 50);
+        assertTrue(weather.getHumidity() >= 0 && weather.getHumidity() <= 100);
+    }
+
+}
