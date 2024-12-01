7a027b5d920132ed4f79e3d26ca58e84b5aff178
lijack16 <jackwl.li@mail.utoronto.ca>
2024-11-30 19:57:11 -0500

    Method openmapInBrowser edited so that it works with hiking spots with special characters in their names.

--- src/main/java/frontend/controller/LocationController.java
+++ src/main/java/frontend/controller/LocationController.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import javax.swing.*;
@@ -151,9 +153,10 @@ public class LocationController {
     }
 
     private void openMapInBrowser(HikingSpot spot) throws URISyntaxException, IOException {
+        final String encodedName = URLEncoder.encode(spot.getName(), StandardCharsets.UTF_8);
         final String mapUrl = String.format("https://www.google.com/maps/search/%s/@%s,%s,12z/data=!3m1"
                         + "!4b1?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%%3D%%3D",
-                spot.getName().replace(" ", "+"), spot.getLatitude(), spot.getLongitude());
+                encodedName, spot.getLatitude(), spot.getLongitude());
         Desktop.getDesktop().browse(new java.net.URI(mapUrl));
     }
 }
