b0d2d92b529523ee449416c65d5aaa7ffed4bbcf
lijack16 <jackwl.li@mail.utoronto.ca>
2024-12-03 23:02:39 -0500

    Deleted unused files.

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -9,12 +9,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 import backend.service.ChatbotService;
 import frontend.controller.ChatbotController;
